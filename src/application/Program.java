40d1f39d67344eda445a461de2012628595bca26
Jaqueline Lins <quelinalins@gmail.com>
2023-11-20 15:20:45 -0300

    Exercício sem correção

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import models.entities.Account;
+import models.exceptions.DomainException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Account number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder name: ");
+            String name = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, name, balance, withdrawLimit);
+
+            System.out.println(account);
+            System.out.println();
+            System.out.print("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+
+            account.withdraw(amount);
+            System.out.println("Updated data");
+            System.out.print(account);
+
+
+        } catch (DomainException e){
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+        catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+        }
+        sc.close();
+    }
+
+}
